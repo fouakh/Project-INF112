753f8bc24d67a4012f962e48634b80794a389cb3
Fouad Khelifi <fouad.khelifi@telecom-paris.fr>
2024-05-17 10:53:35 +0200

    Add loggings

--- projectInf112/src/test/SimulatorApplication.java
+++ projectInf112/src/test/SimulatorApplication.java
@@ -2,13 +2,22 @@ package test;
 
 import fr.tp.inf112.projects.canvas.view.CanvasViewer;
 
+import java.util.logging.Logger;
+import java.util.Arrays;
+
 import implement.*;
 import controller.*;
 
 public class SimulatorApplication {
 
+	private static final Logger LOGGER = Logger.getLogger(SimulatorApplication.class.getName());
+	
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
+		
+		//System.setProperty("java.util.logging.config.file", "config/logging.properties");
+		LOGGER.info("Starting the robot simulator...");
+        LOGGER.config("With parameters " + Arrays.toString(args) + ".");
         
 		Application app = new Application();
 		SimulatorController controller = new SimulatorController(app.getFactory());
